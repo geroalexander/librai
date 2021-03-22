8836b3dee43a77b1da839c656e82cf4778a6a880
Pamela <pmelacodes@gmail.com>
2021-03-22 12:17:46 +0000

    refactor: recommscore

--- server/recommendationScore/recommScore.js
+++ server/recommendationScore/recommScore.js
@@ -1,5 +1,5 @@
 //given a book and a user
-import { getBookById } from '../booksApiService/getBookById';
+const { getBookById } = require('../booksApiService/getBookById');
 
 const books = require('../models/books');
 const getRecommendations = require('../recombeeService/getRecommendations');
@@ -8,19 +8,19 @@ const getCompatScore = async (user, book) => {
   // check the average rating;
   let hasGoodRating = 0;
   if (book.averageRating > 4) hasGoodRating += book.averageRating - 4;
-  
+
   // compare it with favorite genres list
   let isFavoriteGenre = 0;
   const inFavoriteGenres = user.favoriteGenres.some((g) =>
     books.categories.includes(g),
   );
   if (inFavoriteGenres) isFavoriteGenre++;
-  
+
   // compare it with past ratings
   let isSimilarToRatings = 0;
   const ratedBooks = user.books.filter((b) => b.interaction.rating === 1);
   const numOfRated = ratedBooks.length;
-  
+
   ratedBooks.forEach((rated) => {
     let similarityScore = 0;
     const sameGenre = rated.categories.some((c) => books.categories.include(c));
@@ -28,25 +28,25 @@ const getCompatScore = async (user, book) => {
     const sameAuthor = rated.author.some((a) => book.author.include(a));
     if (sameAuthor) similarityScore++;
     if (
-      book.pageCount >= rated.pageCount - 100 && 
+      book.pageCount >= rated.pageCount - 100 &&
       book.pageCount <= rated.pageCount + 100
     )
       similarityScore++;
     if (book.publisher === rated.publisher) similarityScore++;
     if (similarityScore > 2) isSimilarToRatings++;
   });
-  
+
   let similarityWithPast = Math.round((isSimilarToRatings / numOfRated) * 4);
-  
+
   //compare it with recombee suggestions to user
   let similarityWithRecomms = 0;
   const recommBooks = await getRecommendations(user.id, 4);
-  
-  recommBooks.recomms.forEach((rec) => {
+
+  for (const rec of recommBooks.recomms) {
     const retrievedBook = await getBookById(rec.id);
     if (retrievedBook) {
       let similarityScore = 0;
-      const sameGenre = rec.categories.some(c => book.categories.include(c))
+      const sameGenre = rec.categories.some((c) => book.categories.include(c));
       if (sameGenre) similarityScore++;
       const sameAuthor = rec.author.some((a) => book.author.include(a));
       if (sameAuthor) similarityScore++;
@@ -58,11 +58,15 @@ const getCompatScore = async (user, book) => {
       if (book.publisher === rec.publisher) similarityScore++;
       if (similarityScore > 2) similarityWithRecomms++;
     }
-  });
-  
-  const compatabilityScore = hasGoodRating + isFavoriteGenre + similarityWithPast + similarityWithRecomms
-  return compatabilityScore;
-}
+  }
 
-export default getCompatScore;
+  const compatabilityScore =
+    hasGoodRating +
+    isFavoriteGenre +
+    similarityWithPast +
+    similarityWithRecomms;
+  return compatabilityScore;
+};
+
+module.exports = getCompatScore;
 //return score out of 10
