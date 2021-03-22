8b1a5df40dc970db26abd61cedb82010ec200670
Ben Pearce <benpearce9@live.co.uk>
2021-03-22 15:30:07 +0000

    fix(populate.js): made functions async

--- server/npm_scripts/populate.js
+++ server/npm_scripts/populate.js
@@ -12,8 +12,15 @@ const mockUser = {
   lastName: 'Damon',
   email: 'matt_damon@hollywood.com',
   password: bcrypt.hash('password', 10),
-  favoriteGenres: ['Music', 'Performing Arts', 'Health & Fitness', 'Drama', 'Gardening'],
-  profilePic: 'https://upload.wikimedia.org/wikipedia/commons/8/82/Damon_cropped.jpg'
+  favoriteGenres: [
+    'Music',
+    'Performing Arts',
+    'Health & Fitness',
+    'Drama',
+    'Gardening',
+  ],
+  profilePic:
+    'https://upload.wikimedia.org/wikipedia/commons/8/82/Damon_cropped.jpg',
 };
 let mockUserCreated;
 
@@ -22,7 +29,6 @@ let mockUserCreated;
 MAIN SCRIPT
 -----------
 */
-
 
 // Register mock user
 console.log('Beginning to populate');
@@ -43,62 +49,59 @@ HELPER FUNCTIONS
 ---------------- 
 */
 
-function registerMockUser() {
+async function registerMockUser() {
   try {
-    console.log('Attempting to insert user...');
-
-    const existingUser = await user.findOne({ where: { email: mockUser.email } });
+    const existingUser = await user.findOne({
+      where: { email: mockUser.email },
+    });
     if (existingUser) throw new Error('Matt Damon already exists');
 
     mockUserCreated = await user.create(mockUser);
     if (!mockUserCreated) throw new Error('Matt damon could not be created');
-
-    console.log('User inserted successfully');
   } catch (error) {
     console.error(error);
   }
-};
+}
 
-function mockAddSavedBook (book) {
+async function mockAddSavedBook(book) {
   try {
     let targetBook = await book.findByPk(book.id);
     if (!targetBook) targetBook = await book.create(book);
-    await mockUserCreated.addBook(targetBook, { through: { isSaved: true }});
+    await mockUserCreated.addBook(targetBook, { through: { isSaved: true } });
     // TODO: call recombee bookmark function
   } catch (error) {
     console.error(error);
   }
-};
+}
 
 // Function that returns -1, 0, or 1 depending on math random
-function randomiseRating () {
+function randomiseRating() {
   let random = Math.random();
   let rating;
 
   if (random < 0.33) rating = -1;
-  else if(random < 0.66) rating = 0;
+  else if (random < 0.66) rating = 0;
   else rating = 1;
 
   return rating;
-};
+}
 
-function mockAddRatedBook (book) {
+async function mockAddRatedBook(book) {
   const rating = randomiseRating();
   try {
     let targetBook = await book.findByPk(book.id);
     if (!targetBook) targetBook = await book.create(book);
 
     const targetInteraction = await interaction.findOne({
-      where: {userId: mockUserCreated.id, bookId: book.id},
+      where: { userId: mockUserCreated.id, bookId: book.id },
     });
     if (targetInteraction) {
       await targetInteraction.update({ rating });
-      return
-    };
-    await mockUserCreated.addBook(book, { through: { rating }});
-    // TODO: call recombee add rating 
+      return;
+    }
+    await mockUserCreated.addBook(book, { through: { rating } });
+    // TODO: call recombee add rating
   } catch (error) {
     console.error(error);
   }
-};
-
+}
