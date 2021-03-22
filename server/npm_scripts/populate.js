e3bfbf9f6b1b3551e7ba76422c4fc6a38fcfee26
Ben Pearce <benpearce9@live.co.uk>
2021-03-22 11:37:01 +0000

    feature(npm populate script): add initial register user function

--- server/npm_scripts/populate.js
+++ server/npm_scripts/populate.js
@@ -1,3 +1,31 @@
 // populate.js
-// connects to our DB, populates it with: 1 user who has 10 saved, 10 rated books
+// populates PSQL DB it with: 1 user who has 10 saved, 10 rated books
 // execute: npm run populate
+
+const { models } = require('../models/');
+const { user, book, interaction } = models;
+const bcrypt = require('bcrypt');
+
+const mockUser = {
+  firstName: 'Matt',
+  lastName: 'Damon',
+  email: 'matt_damon@hollywood.com',
+  password: bcrypt.hash('password', 10),
+  favoriteGenres: ['Music', 'Performing Arts', 'Health & Fitness', 'Drama', 'Gardening'],
+  profilePic: 'https://upload.wikimedia.org/wikipedia/commons/8/82/Damon_cropped.jpg'
+};
+
+const registerMockUser = () => {
+  try {
+    const existingUser = await user.findOne({ where: { email: mockUser.email } });
+    if (existingUser) throw new Error('Matt Damon already exists');
+
+    const mockUserWasCreated = await user.create(mockUser);
+    if (!mockUserWasCreated) throw new Error('Matt damon could not be created');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+registerMockUser();
+
