3023dd8412da8cd46c746fccb14e71299b13776e
Ben Pearce <benpearce9@live.co.uk>
2021-03-23 19:37:58 +0000

    feat(ProfileScreen): final commit for tonight, minor changes

--- client/src/Components/Profile/ProfileScreen.tsx
+++ client/src/Components/Profile/ProfileScreen.tsx
@@ -5,32 +5,29 @@ import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { User } from '../../Interfaces/userObject';
 import { RootState } from '../../index';
 
-// export interface User {
-//   id: string;
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   profilePic: string | null;
-//   favoriteGenres: string[] | [];
-//   books?: Book[] | null;
-// }
-
 interface ProfileScreenProps extends RouteComponentProps {}
+// const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2UyZTQ2MC0zMDU1LTQwMGItOGQ4OC0zMTE2ODA4NzU3NzQiLCJpYXQiOjE2MTY1Mjc5NjJ9.yID2_occRk36cc1VyyIGlBeauu_DGtKxNr7Aoxxvm98';
 
 const ProfileScreen: React.FC<ProfileScreenProps> = (props) => {
   const user: User = useSelector(
     (state: RootState) => state.userReducer?.userWithBooks
   );
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const renderFavoriteGenres = () => {};
+  // const renderFavoriteGenres = user.favoriteGenres.map((genre: string) => {});
 
-  const renderBooks;
+  // const renderBooks = user.books.map(book => <BookDetail book={book} />);
 
   return (
     <div>
       <button>Logout</button>
       <img src={String(user.profilePic)} alt="" />
+      <h1>
+        {user.firstName} {user.lastName}
+      </h1>
+      <p>{user.email}</p>
+      {/* <p>{renderFavoriteGenres}</p> */}
+      {/* renderBooks */}
     </div>
   );
 };
