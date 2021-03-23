b827d3e751c6c2c0c657667791eadf1521dfeeb7
Ben Pearce <benpearce9@live.co.uk>
2021-03-23 18:13:54 +0000

    intermediate commit to pull changes

--- client/src/Components/Profile/ProfileScreen.tsx
+++ client/src/Components/Profile/ProfileScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { User } from '../../Interfaces/userObject';
-import { RootState } from '../../App';
+import { RootState } from '../../index';
 
 // export interface User {
 //   id: string;
