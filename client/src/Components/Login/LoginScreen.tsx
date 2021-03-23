fde7ad2feffd27578b66e20c876f333fa7c8d425
Gero <gero.a.kassing@gmail.com>
2021-03-23 19:01:28 +0000

    feat(login): inital login

---
+++ client/src/Components/Login/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div className="login-container">
+      <div className="login-content">
+        <h1 className="title">Login</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
