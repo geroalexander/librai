26adff3c0408c3efd1ddc0d8e3989bbf4ea77262
Pamela <pmelacodes@gmail.com>
2021-03-23 18:17:39 +0000

    fix: routes.tsx

--- client/src/Routes.ts
+++ client/src/Routes.ts
@@ -3,13 +3,4 @@ import Profile from './Components/Profile/ProfileScreen';
 import Saved from './Components/Saved/SavedScreen';
 import BookDetails from './Components/BookDetails/BookDetailsScreen';
 
-<<<<<<< HEAD
 export { Dashboard, Profile, Saved, BookDetails };
-=======
-export {
-  Dashboard,
-  Profile,
-  Saved,
-  BookDetails,
-};
->>>>>>> 942fc44ddadba7ec64c52fda41b6e4206eb09c3a
