0f34129843f2c71cceb065979ccfa314a05a7d83
Ben Pearce <benpearce9@live.co.uk>
2021-03-23 11:52:36 +0000

    feat(redux-actions/auth): add login, register, logout actions && create form interface

---
+++ client/src/Interfaces/formObject.tsx
@@ -0,0 +1,6 @@
+export interface Form {
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+}
--- client/src/Store/actions/auth.ts
+++ client/src/Store/actions/auth.ts
@@ -1,3 +1,36 @@
 import { register, login, addFormInfo, logout } from '../../ApiClientService/Auth';
+import { User } from '../../Interfaces/user';
+import { AppDispatch } from '../../App';
+import { SET_LOGIN, SET_LOGOUT } from './ActionTypes';
 
+export const setLogin = (user: User) => async (dispatch: AppDispatch) => {
+  const { email, password } = user;
+  const {accessToken} = await login(email, password);
+  localStorage.setItem('accessToken', accessToken);
+  dispatch({
+    type: SET_LOGIN,
+    payload: {
+      isLoggedIn: true,
+    },
+  });
+};
 
+export const setLogout = () => async (dispatch: AppDispatch) => {
+  const accessToken: string = localStorage.getItem('accessToken');
+  localStorage.removeItem('accessToken');
+  await logout(accessToken);
+  dispatch({
+    type: SET_LOGOUT,
+    payload: {
+      isLoggedIn: false,
+    },
+  });
+};
+
+const setRegister = () => async (dispatch: AppDispatch) => {
+  
+}
+
+const setAddFormInfo = () => async (dispatch: AppDispatch) => {
+  
+}
