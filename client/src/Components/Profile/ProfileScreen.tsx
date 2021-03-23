3f19c71a1e9f0dcf22077d9df6314809cc8dc355
Ben Pearce <benpearce9@live.co.uk>
2021-03-23 17:24:09 +0000

    feature(ProfileScreen.tsx): initial imports and setup

--- client/src/Components/Profile/ProfileScreen.tsx
+++ client/src/Components/Profile/ProfileScreen.tsx
@@ -1,1 +1,11 @@
 //BEN
+import React from 'react';
+import {useSelector, useDispatch } from 'react-redux';
+import { User } from '../../Interfaces/userObject';
+import { RootState } from '../../App';
+
+const ProfileScreen = () => {
+  const user: User = useSelector((state: RootState) => state.userReducer?.userWithBooks);
+}
+
+export default ProfileScreen;
