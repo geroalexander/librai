c1efdcc591f9bb9571bad235f54edc56054385f5
Pamela <pmelacodes@gmail.com>
2021-03-24 12:31:48 +0000

    feat: update dashboard styles

--- client/src/Components/Dashboard/DashboardScreen.tsx
+++ client/src/Components/Dashboard/DashboardScreen.tsx
@@ -47,18 +47,16 @@ const Dashboard: React.FC<DashboardScreenProps> = () => {
     return (
       <div className="dashboard">
         <div className="bookwrapper">
-          <p className="title">Recently saved</p>
+          <p className="title">Recommended</p>
           <div className="booklist">
-            {books
-              .filter((b: Book) => b.interaction.isSaved)
-              .map((book: Book) => (
-                <div className="book-preview">
-                  <img
-                    src={book.thumbnail ? book.thumbnail : undefined}
-                    alt={book.title}
-                  />
-                </div>
-              ))}
+            {recommended.map((book: Book) => (
+              <div className="book-preview">
+                <img
+                  src={book.thumbnail ? book.thumbnail : undefined}
+                  alt={book.title}
+                />
+              </div>
+            ))}
           </div>
         </div>
         <div className="bookwrapper-small">
@@ -77,16 +75,18 @@ const Dashboard: React.FC<DashboardScreenProps> = () => {
           </div>
         </div>
         <div className="bookwrapper">
-          <p className="title">Recommended</p>
+          <p className="title">Recently saved</p>
           <div className="booklist">
-            {recommended.map((book: Book) => (
-              <div className="book-preview">
-                <img
-                  src={book.thumbnail ? book.thumbnail : undefined}
-                  alt={book.title}
-                />
-              </div>
-            ))}
+            {books
+              .filter((b: Book) => b.interaction.isSaved)
+              .map((book: Book) => (
+                <div className="book-preview">
+                  <img
+                    src={book.thumbnail ? book.thumbnail : undefined}
+                    alt={book.title}
+                  />
+                </div>
+              ))}
           </div>
         </div>
       </div>
