a8d44eb83cc0a9b915955621be20ef6ab6788d39
Ben Pearce <benpearce9@live.co.uk>
2021-03-24 14:19:39 +0000

    intermediate commit to pull from development

--- client/src/Components/Profile/ProfileScreen.tsx
+++ client/src/Components/Profile/ProfileScreen.tsx
@@ -1,9 +1,10 @@
 //BEN
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { User } from '../../Interfaces/userObject';
 import { RootState } from '../../index';
+import { _getUserWithBooks } from '../../Store/actions/users';
 
 interface ProfileScreenProps extends RouteComponentProps {}
 
@@ -11,23 +12,32 @@ const ProfileScreen: React.FC<ProfileScreenProps> = (props) => {
   const user: User = useSelector(
     (state: RootState) => state.userReducer?.userWithBooks
   );
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  const renderFavoriteGenres = user.favoriteGenres.map(
-    (genre: string, i: number) => <p key={i}>{genre}</p>
-  );
+  // const renderFavoriteGenres = user.favoriteGenres.map(
+  //   (genre: string, i: number) => <p key={i}>{genre}</p>
+  // );
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const action = await _getUserWithBooks();
+      dispatch(action);
+    };
+
+    getBooks();
+  }, []);
+
+  console.log('USERUSERUSER', user.books);
 
   // const renderBooks = user.books.map(book => <BookDetail key={book.id} book={book} />);
 
   return (
     <div>
       <button>Logout</button>
-      <img src={String(user.profilePic)} alt="" />
-      <h1>
-        {user.firstName} {user.lastName}
-      </h1>
-      <p>{user.email}</p>
-      <p>{renderFavoriteGenres}</p>
+      {/* <img src={String(user.profilePic)} alt="" /> */}
+      <h1>{/* {user.firstName} {user.lastName} */}</h1>
+      {/* <p>{user.email}</p> */}
+      {/* <p>{renderFavoriteGenres}</p> */}
       {/* renderBooks */}
     </div>
   );
