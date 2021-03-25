e36fa9017e4163a66e38e67ae12e1dbeef6c682c
Gero <gero.a.kassing@gmail.com>
2021-03-25 10:19:29 +0000

    fix(login/register) styles completed

--- client/src/Components/Register/RegisterScreen.tsx
+++ client/src/Components/Register/RegisterScreen.tsx
@@ -4,6 +4,10 @@ import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../..';
 import './Register.css';
 import { setRegister } from '../../Store/actions/auth';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import PersonIcon from '@material-ui/icons/Person';
+import LockIcon from '@material-ui/icons/Lock';
+import EmailIcon from '@material-ui/icons/Email';
 
 const Register: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -28,42 +32,61 @@ const Register: React.FC = () => {
         <h2 className="title">Register</h2>
         {/*ERROR*/}
         <div className="form-group">
-          <label className="firstName">First Name</label>
+          <div className="icon">
+            <PersonOutlineIcon style={{ color: '#fffef9' }}></PersonOutlineIcon>
+          </div>
           <input
+            className="input"
             onChange={(e) => setFirstName(e.target.value)}
             type="text"
             name="firstName"
+            placeholder="FIRST NAME"
             id="firstName"
           />
         </div>
         <div className="form-group">
-          <label className="lastName">Last Name</label>
+          <div className="icon">
+            <PersonIcon style={{ color: '#fffef9' }}></PersonIcon>
+          </div>
           <input
+            className="input"
             onChange={(e) => setLastName(e.target.value)}
             type="text"
             name="lastName"
+            placeholder="LAST NAME"
             id="lastName"
           />
         </div>
         <div className="form-group">
-          <label className="email">Email</label>
+          <div className="icon">
+            <EmailIcon style={{ color: '#fffef9' }}></EmailIcon>
+          </div>
           <input
+            className="input"
             onChange={(e) => setEmail(e.target.value)}
             type="text"
             name="email"
+            placeholder="EMAIL"
             id="email"
           />
         </div>
         <div className="form-group">
-          <label className="password">Password</label>
+          <div className="icon">
+            <LockIcon style={{ color: '#fffef9' }}></LockIcon>
+          </div>
           <input
+            className="input"
             onChange={(e) => setPassword(e.target.value)}
             type="password"
             name="password"
+            placeholder="PASSWORD"
             id="password"
           />
         </div>
-        <input type="submit" value="LOGIN" />
+        <input className="submitButton" type="submit" value="SIGN UP" />
+        <a href="" className="to-login">
+          Already have an account? Click here!
+        </a>
       </div>
     </form>
   );
