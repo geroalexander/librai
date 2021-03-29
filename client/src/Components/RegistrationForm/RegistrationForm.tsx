93da74426f4187613f5ec5ac0dc24dd64880dad2
Ben Pearce <benpearce9@live.co.uk>
2021-03-29 16:34:09 +0100

    chore(RegistrationForm.tsx): modify to use reusable ErrorMessage component

--- client/src/Components/RegistrationForm/RegistrationForm.tsx
+++ client/src/Components/RegistrationForm/RegistrationForm.tsx
@@ -11,17 +11,7 @@ import { _registrationForm } from '../../Store/actions/users';
 import { popularBooks } from './popular_books.js';
 import { PopularBook } from '../../Interfaces/popularBookObject';
 import BookStatusBar from '../BookDetails/BookStatusBar/BookStatusBar';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
-import { TransitionProps } from '@material-ui/core/transitions';
-
-const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & { children?: React.ReactElement<any, any> },
-  ref: React.Ref<unknown>
-) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
+import ErrorMessage from '../Shared/ErrorMessage';
 
 interface RegistrationFormProps extends RouteComponentProps {}
 
@@ -30,21 +20,12 @@ const RegistrationForm: React.FC<RegistrationFormProps> = (props) => {
   const [favoriteGenres, setFavoriteGenres] = useState<string[]>([]);
   const [favoriteBooks, setFavoriteBooks] = useState<PopularBook[]>([]);
   const [open, setOpen] = React.useState(false);
-  console.log('HERE', open);
 
   const dispatch = useDispatch();
   const history = useHistory();
 
   const genreErrorMessage = 'Please pick between 3 and 5 genres';
   const bookErrorMessage = 'Please pick at least 3 books';
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
 
   const addFavoriteGenre = (e: any) => {
     const genreToAdd = e.target.innerText;
@@ -71,7 +52,7 @@ const RegistrationForm: React.FC<RegistrationFormProps> = (props) => {
   const handleSubmitGenres = () => {
     const numberOfGenres = favoriteGenres.length;
     if (numberOfGenres < 3 || numberOfGenres > 5) {
-      handleClickOpen();
+      setOpen(true);
       return;
     }
     setIsUserPickingGenres(false);
@@ -79,7 +60,7 @@ const RegistrationForm: React.FC<RegistrationFormProps> = (props) => {
 
   const handleSubmitForm = async () => {
     if (favoriteBooks.length < 3) {
-      handleClickOpen();
+      setOpen(true);
       return;
     }
     const action = await _registrationForm(favoriteBooks, favoriteGenres);
@@ -150,18 +131,13 @@ const RegistrationForm: React.FC<RegistrationFormProps> = (props) => {
           </Button>
         </div>
       )}
-      <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle id="alert-dialog-slide-title">
-          {isUserPickingGenres ? genreErrorMessage : bookErrorMessage}
-        </DialogTitle>
-      </Dialog>
+      {open && (
+        <ErrorMessage
+          message={isUserPickingGenres ? genreErrorMessage : bookErrorMessage}
+          open={open}
+          setOpen={setOpen}
+        />
+      )}
     </div>
   );
 };
