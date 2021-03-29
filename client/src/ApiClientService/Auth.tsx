ace4698c3009d86b592202522fadbe129dca68da
Ben Pearce <benpearce9@live.co.uk>
2021-03-29 11:34:24 +0100

    feat(ApiClioentService/fetchRequest.tsx): add global fetch request function

--- client/src/ApiClientService/Auth.tsx
+++ client/src/ApiClientService/Auth.tsx
@@ -1,4 +1,4 @@
-const { REACT_APP_BASE_URL } = process.env;
+import { fetchRequest } from './fetchRequest';
 
 const register = (
   firstName: string,
@@ -82,17 +82,6 @@ const logout = (accessToken: string) => {
   //   },
   // }).catch((err) => console.log('error with logout', err));
 };
-
-async function fetchRequest(path: string, options: RequestInit) {
-  const url: RequestInfo = REACT_APP_BASE_URL + path;
-  const res = await fetch(url, options);
-  if (res.ok) return await res.json();
-  else {
-    const { message } = await res.json();
-    console.error(`Error with path: ${path}`, message);
-    return Promise.reject(new Error(message));
-  }
-}
 
 // single client function(endpoint, options)
 
---
+++ client/src/ApiClientService/fetchRequest.tsx
@@ -0,0 +1,14 @@
+const { REACT_APP_BASE_URL } = process.env;
+
+async function fetchRequest(path: string, options: RequestInit) {
+  const url: RequestInfo = REACT_APP_BASE_URL + path;
+  const res = await fetch(url, options);
+  if (res.ok) return await res.json();
+  else {
+    const { message } = await res.json();
+    console.error(`Error with path: ${path}`, message);
+    return Promise.reject(new Error(message));
+  }
+}
+
+export { fetchRequest };
