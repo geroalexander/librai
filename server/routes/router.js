224623ad07c1c9e42caaaafac76a370fa2773f02
Pamela Chen <pamelakaylin@gmail.com>
2021-03-30 18:17:23 +0100

    feat: changed google credentials location

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,6 @@
 'use strict';
 const { Router } = require('express');
 const router = Router();
-// const authMiddleware = require('./auth/authMiddleware'); // not sure if needed
 
 router.use('/user', require('./user_routes'));
 router.use('/book', require('./book_routes'));
