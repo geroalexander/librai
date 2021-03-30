4530d8c8b147a89dbfd114a5e9f1a2070b511855
Pamela Chen <pamelakaylin@gmail.com>
2021-03-30 15:39:00 +0100

    feat: update server models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,13 +2,23 @@
 
 require('dotenv').config({ path: __dirname + '/../../.env' });
 const config = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_PORT, DB_HOST, DB_DIALECT } = config;
 const { Sequelize } = require('sequelize');
 const associations = require('./associations');
-const { DATABASE_URL } = config;
-// const { DB_NAME, DB_USER, DB_PASSWORD, DB_PORT, DB_HOST, DB_DIALECT } = config;
+const DATABASE_URL =
+  process.env.DATABASE_URL ||
+  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
-console.log(DATABASE_URL, 'DATABASEURL');
 const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  ssl: process.env.DATABASE_URL,
+  dialectOptions: {
+    ssl: process.env.DATABASE_URL && {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   pool: {
     max: 5,
     min: 0,
