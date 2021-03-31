22a4660afb5f319c87fb06a3acfddd71e8d3c912
Andras <65424110+andrasium@users.noreply.github.com>
2021-03-31 17:29:01 +0100

    feat: popup added to BookStatusBar to inform users about compatibility score

--- client/src/Components/BookDetails/BookStatusBar/BookStatusBar.tsx
+++ client/src/Components/BookDetails/BookStatusBar/BookStatusBar.tsx
@@ -23,18 +23,67 @@ import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt'
 import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
 import DoneAllRoundedIcon from '@material-ui/icons/DoneAllRounded';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { isMobile } from 'react-device-detect';
 
 interface BookStatusBarProps extends RouteComponentProps {
   book: Book;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    popover: {
+      pointerEvents: 'none',
+    },
+    paper: {
+      padding: theme.spacing(1),
+    },
+    typography: {
+      padding: theme.spacing(2),
+    },
+    "@keyframes enter": {
+      "0%": {
+        transform: "scale(0)",
+        opacity: 0
+      },
+      "100%": {
+        transform: "scale(0)",
+        opacity: 0
+      }
+    }
+    })
+);
+
 const BookStatusBar: React.FC<BookStatusBarProps> = ({ book }) => {
+  const classes = useStyles();
   const [isSaved, setIsSaved] = useState(false);
   const [rated, setRated] = useState(false);
   const [rating, setRating] = useState<any>(null);
-
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
   const dispatch = useDispatch();
+
+  const handlePopoverOpen = (
+    event: React.MouseEvent<HTMLElement, MouseEvent>
+  ) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   useEffect(() => {
     if (book.interaction) {
@@ -152,7 +201,70 @@ const BookStatusBar: React.FC<BookStatusBarProps> = ({ book }) => {
       </div>
       {!rated && (
         <>
-          <div className="compat-score">{handleCompatScore()}</div>
+          <div className="compat-score">
+            {isMobile ? (
+              <div>
+                <Button
+                  aria-describedby={id}
+                  variant="contained"
+                  onClick={handleClick}
+                  style={{ background: 'transparent', boxShadow: 'none' }}
+                >
+                  {handleCompatScore()}
+                </Button>
+                <Popover
+                  id={id}
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+                >
+                  <Typography className={classes.typography}>
+                    Your personalized compatability score!
+                  </Typography>
+                </Popover>
+              </div>
+            ) : (
+              <>
+                <Typography
+                  aria-owns={open ? 'mouse-over-popover' : undefined}
+                  aria-haspopup="true"
+                  onMouseEnter={handlePopoverOpen}
+                  onMouseLeave={handlePopoverClose}
+                >
+                  {handleCompatScore()}
+                </Typography>
+                <Popover
+                  id="mouse-over-popover"
+                  className={classes.popover}
+                  classes={{
+                    paper: classes.paper,
+                  }}
+                  open={open}
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+                  onClose={handlePopoverClose}
+                  disableRestoreFocus
+                >
+                  Your personalized compatability score!
+                </Popover>
+              </>
+            )}
+          </div>
           <div className="bookmark-wrapper">
             <FloatingMenu
               slideSpeed={500}
